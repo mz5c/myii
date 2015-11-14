4a1da8c32552c9855e1b9817ae83bc326c6bc56b
wucheng.wu <w11w23c58@126.com>
2015-11-14 12:11:18 +0800

    learn yii

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -106,4 +106,8 @@ class SiteController extends Controller
 		Yii::app()->user->logout();
 		$this->redirect(Yii::app()->homeUrl);
 	}
+
+	public function actionHello($words = 'hello world'){
+		echo 'what you want to say : '.$words.'<br>';
+	}
 }
