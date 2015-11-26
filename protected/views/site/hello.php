c4149af33a5ca8c5e8ab3afd9d7fbaf11e7ce14a
wucheng.wu <w11w23c58@126.com>
2015-11-26 10:13:31 +0800

    init

---
+++ protected/views/site/hello.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mz5c
+ * Date: 2015/11/14
+ * Time: 12:57
+ */
+$this->pageTitle=Yii::app()->name.' - Hello';
+$this->breadcrumbs=array(
+    'Hello',
+);?>
+<h1>Hello</h1>
+<?php echo 'what you want to say : '.$words.'<br>';
+?>
