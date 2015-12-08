de575c9c0591703ac0f2d78fb28a60b34017d22a
wucheng.wu <w11w23c58@126.com>
2015-12-08 10:35:55 +0800

    add user

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -21,6 +21,7 @@ class UserIdentity extends CUserIdentity
 			// username => password
 			'demo'=>'demo',
 			'admin'=>'admin',
+			'rico'=>'rico'
 		);
 		if(!isset($users[$this->username]))
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
